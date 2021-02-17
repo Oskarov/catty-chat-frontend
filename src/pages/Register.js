b9fed97eb3443276e3f2bf6b76719fb99f3790e7
Di <elven.legacy@hotmail.com>
2021-02-17 21:44:41 +0300

    Registration form

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import {Button, Container, Form} from "react-bootstrap";
+import {useForm} from "../util/hooks";
 
 function Register(props) {
 
+
+
+    const { onChange, onSubmit, values } = useForm( registerUser, {
+        email: '',
+        username: '',
+        password: '',
+        confirmedPassword: ''
+    })
+
+    const submitRegisterForm = (e) =>{
+        e.preventDefault();
+    }
+
+    function registerUser(){
+        submitRegisterForm();
+    }
 
     return (
         <div>
@@ -10,7 +27,7 @@ function Register(props) {
                 <Form>
                     <Form.Group>
                         <Form.Label>Email адрес</Form.Label>
-                        <Form.Control type="email" placeholder="catty@evil.cat" />
+                        <Form.Control type="email" name="email" placeholder="catty@evil.cat" value={values.email} onChange={onChange}/>
                         <Form.Text className="text-muted">
                             Никому не сообщайте ваши реквизиты для входа.
                         </Form.Text>
@@ -18,17 +35,17 @@ function Register(props) {
 
                     <Form.Group>
                         <Form.Label>Имя пользователя</Form.Label>
-                        <Form.Control type="text" placeholder="Catty15" />
+                        <Form.Control type="text" name="username" placeholder="Catty15" value={values.username} onChange={onChange}/>
                     </Form.Group>
 
                     <Form.Group>
                         <Form.Label>Пароль</Form.Label>
-                        <Form.Control type="password" placeholder="********" />
+                        <Form.Control type="password" name="password" autoComplete="new-password" placeholder="********" value={values.password} onChange={onChange}/>
                     </Form.Group>
 
                     <Form.Group>
                         <Form.Label>Пароль ещё раз</Form.Label>
-                        <Form.Control type="password" placeholder="********" />
+                        <Form.Control type="password" name="confirmedPassword" autoComplete="off" placeholder="********" value={values.confirmedPassword} onChange={onChange}/>
                     </Form.Group>
 
                     <Button variant="primary" type="submit">
---
+++ src/util/hooks.js
@@ -0,0 +1,36 @@
+import {useState} from 'react';
+
+export const useForm = (callback, initialState = {}) => {
+    const [values, setValues] = useState(initialState);
+
+    const onChange = (e) => {
+        const element = e.target;
+        setValues({
+            ...values,
+            [element.name]: element.value
+        });
+        console.log(values);
+    }
+
+    const changeBunchValues = (bunch) =>{
+        setValues({
+            ...values,
+            ...bunch
+        });
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        callback();
+    }
+
+    return {
+        onChange,
+        onSubmit,
+        changeBunchValues,
+        values
+    }
+}
+
+
+
