2a988c20b5ac43cb70d8a8cc42332fab11c601e3
Di <elven.legacy@hotmail.com>
2021-03-11 22:29:15 +0300

    Subscription init

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import App from './App';
 import ApolloClient from "apollo-client";
+import {split} from '@apollo/client'
+import {getMainDefinition} from '@apollo/client/utilities'
 import {InMemoryCache} from "apollo-cache-inmemory";
 import {createHttpLink} from "apollo-link-http";
 import {ApolloProvider} from '@apollo/react-hooks';
 import {setContext} from "apollo-link-context";
 import {AuthProvider} from "./context/auth";
 import {MessagesProvider} from "./context/message";
+import {WebSocketLink} from '@apollo/client/link/ws';
 
-const httpLink = createHttpLink({
+let httpLink = createHttpLink({
     uri: 'http://localhost:4000/'
 });
+
+
 const authLink = setContext((req, pre) => {
     const token = localStorage.getItem('jwtToken');
     return {
@@ -20,8 +25,32 @@ const authLink = setContext((req, pre) => {
     }
 })
 
+httpLink = authLink.concat(httpLink);
+
+const token = localStorage.getItem('jwtToken');
+const wsLink = new WebSocketLink({
+    uri: 'ws://localhost:4000/graphql',
+    options: {
+        reconnect: true,
+        connectionParams: {
+            Authorization: token ? `Bearer ${token}` : '',
+        },
+    }
+});
+
+const splitLink = split(
+    ({query}) => {
+        const definition = getMainDefinition(query);
+        return (
+            definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
+        )
+    },
+    wsLink,
+    httpLink,
+);
+
 const client = new ApolloClient({
-    link: authLink.concat(httpLink),
+    link: splitLink,
     cache: new InMemoryCache()
 })
 
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import UserList from "../components/UserList";
 import Messages from "../components/Messages";
+import gql from "graphql-tag";
+import {useSubscription} from "@apollo/client";
 
 function Main(props) {
-   return (
+
+    const {data: messageData, error: messageError} = useSubscription(NEW_MESSAGE);
+
+    useEffect(() => {
+        if (messageError) console.log(messageError);
+        if (messageData) console.log(messageData)
+    }, [messageData, messageError])
+
+    return (
         <div className="chat-body">
             <div className="row">
-                    <UserList />
-                    <Messages />
+                <UserList/>
+                <Messages/>
             </div>
         </div>
     );
 }
+
+const NEW_MESSAGE = gql`
+    subscription  newMessage{
+        newMessage {
+            uuid
+            from
+            to
+            content
+            createdAt
+        }
+    }
+`
 
 export default Main;
