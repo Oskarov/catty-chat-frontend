d4106455d8e1fa40d6a309ee7417080bd4073eee
Di <elven.legacy@hotmail.com>
2021-03-14 21:49:03 +0300

    start working on reactions

--- src/components/MessageItem.js
+++ src/components/MessageItem.js
@@ -1,13 +1,45 @@
-import {useContext} from "react";
+import {useContext, useState} from "react";
 import {AuthContext} from "../context/auth";
+import {Button, OverlayTrigger, Popover} from "react-bootstrap";
 
-function MessageItem({message}){
+function MessageItem({message}) {
     const {user} = useContext(AuthContext);
     const containerClassName = user.username == message.from ? 'message-container __current-user' : 'message-container __other-user';
+    const reactions = ['❤️', '😆', '😯', '😢', '😡', '👍', '👎']
+    const [showPopover, setShowPopover] = useState(false);
+
+    const reactAction = (reaction) => {
+        setShowPopover(false);
+        
+    }
+
+    const reactButton =
+        <OverlayTrigger
+            trigger="click"
+            placement="top"
+            show={showPopover}
+            onToggle={setShowPopover}
+            transition={false}
+            overlay={
+                <Popover classname="rounded-pill">
+                    <Popover.Content>
+                        {reactions.map(reaction=>(
+                          <Button variant="link" key={reaction} onClick={()=> reactAction(reaction)}>{reaction}</Button>
+                        ))}
+                    </Popover.Content>
+                </Popover>
+            }
+        >
+            <Button variant="link" className={user.username == message.from ? 'message-react __user' : 'message-react __other'}>
+                +
+            </Button>
+        </OverlayTrigger>
+
 
     let messageMarc =
         <div className={containerClassName}>
             <p key={message.uuid}>
+                {reactButton}
                 {message.content}
             </p>
         </div>
